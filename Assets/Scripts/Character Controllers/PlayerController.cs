65dbc6cd2c59810f53d974668417f958bd3ee432
Ezydenias <kleinermaximilian@googlemail.com>
2020-02-07 17:15:33 +0100

    Leanring to Swim
    Added Test Puddle to Swim,
    Added Trigger and States to see if object is in the Water or on a Ladder,
    Added a State to see if the Character, once it is in the water, is in fact in a swimming area, otherwise it is in a non swimmer area thus keeps walking,
    Added Floating behaivor with damping the y movement in water

--- Assets/Scripts/Character Controllers/PlayerController.cs
+++ Assets/Scripts/Character Controllers/PlayerController.cs
@@ -8,8 +8,23 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityScale;
     public float rotateSpeed;
+    public float playerHeight = 1;
+    public float floatingSpeed = 3;
+    public float sinkLevel = .5f;
+    public float swimsinklevel = .5f;
+    [Range(1.01f, 2)] public float waterDamper=2;
+    [Range(1.01f, 4)] public float DampingMultiplyer=2;
+
+    private bool grounded = true;
     private CharacterController Controller;
     private Vector3 moveDirection;
+    private float waterTable = -100;
+   
+
+    private bool inWater = false;
+    private bool onLadder = false;
+    private bool swimming = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +35,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grounded = isGrounded();
         var yStore = moveDirection.y;
         moveDirection = transform.forward * Input.GetAxis("Vertical") +
                         transform.right * Input.GetAxis("Horizontal");
@@ -27,15 +43,110 @@ public class PlayerController : MonoBehaviour
         moveDirection *= moveSpeed;
         moveDirection.y = yStore;
 
-        if (Controller.isGrounded)
+        if (inWater)
         {
-            moveDirection.y = 0f;
-            if (Input.GetButtonDown("Jump"))
-                moveDirection.y = jumpForce;
+            if (waterTable > transform.position.y)
+            {
+                swimming = true;
+            }
+            else
+            {
+
+                swimming = false;
+            }
+        }
+        else
+        {
+            swimming = false;
         }
 
 
-        moveDirection.y = moveDirection.y + Physics.gravity.y * Time.deltaTime * gravityScale;
-        Controller.Move(moveDirection * Time.deltaTime);
+        if (onLadder)
+        {
+        }
+        else if (inWater && swimming)
+        {
+            if (waterTable+ sinkLevel > transform.position.y)
+            {
+                moveDirection.y = moveDirection.y - DampingMultiplyer*Time.deltaTime*(moveDirection.y / waterDamper);
+                moveDirection.y = moveDirection.y - Physics.gravity.y * Time.deltaTime *(Mathf.Abs(waterTable - (transform.position.y - swimsinklevel)));
+            }
+            else
+            {
+                moveDirection.y = moveDirection.y - DampingMultiplyer*Time.deltaTime * (moveDirection.y / waterDamper);
+                moveDirection.y = moveDirection.y + Physics.gravity.y * Time.deltaTime * (Mathf.Abs(waterTable - (transform.position.y - swimsinklevel)));
+
+            }
+
+            Controller.Move(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            if (Controller.isGrounded)
+            {
+                moveDirection.y = 0f;
+                if (Input.GetButtonDown("Jump"))
+                    moveDirection.y = jumpForce;
+            }
+
+
+            moveDirection.y = moveDirection.y + Physics.gravity.y * Time.deltaTime * gravityScale;
+            Controller.Move(moveDirection * Time.deltaTime);
+        }
+    }
+
+    public bool getGrounded()
+    {
+        return grounded;
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit[] ground;
+        ground = Physics.RaycastAll(transform.position, new Vector3(0, -1, 0), playerHeight);
+        //Debug.Log("here");
+
+        if (ground.Length >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Filter by using specific layers for this object and "others" instead of using tags
+        if (other.tag == "Water")
+        {
+            waterTable = other.gameObject.transform.position.y - sinkLevel;
+
+            inWater = true;
+        }
+
+        if (other.tag == "Ladder")
+        {
+            onLadder = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("and gone");
+        // Filter by using specific layers for this object and "others" instead of using tags
+//        Debug.Log("left");
+        if (other.tag == "Water")
+        {
+            inWater = false;
+            waterTable = -100;
+        }
+
+        if (other.tag == "Ladder")
+        {
+            onLadder = false;
+        }
     }
 }
