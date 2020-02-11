8e2796ecd5c595b0a353bf02c90312064d3f09ed
Ezydenias <kleinermaximilian@googlemail.com>
2020-02-11 22:02:25 +0100

    Tested Posibilities to de/activate objects
    In futujre reales this can help to add costumes etc for the ccharacters aswell as switching between them. Since every mesh is super Small it would be wiser to instanciate them at gamestart, rather safe load times than using memory conservatively.

---
+++ Assets/test stuff/SpawnBow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> Enable = new List<GameObject>();
+    public List<GameObject> Disable = new List<GameObject>();
+    void OnTriggerEnter(Collider other)
+    {
+        // Filter by using specific layers for this object and "others" instead of using tags
+        if (other.name == "Ezy")
+        {
+            for (int i = 0; i < Enable.Count; i++)
+            {
+                Enable[i].SetActive(true);
+            }
+
+            for (int i = 0; i < Disable.Count; i++)
+            {
+                Disable[i].SetActive(false);
+            }
+        }
+    }
+}
